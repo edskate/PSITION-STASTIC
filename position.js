c7ebd173f4924e491edea58ee49f7ffdf8f6a7f9
Edilson Gafanhoto <68605911+edskate@users.noreply.github.com>
2021-10-22 13:46:18 -0300

    PSITION STASTIC
    PSITION STASTIC

---
+++ position.js
@@ -0,0 +1,13 @@
+const $slementExamplefirst = document.querySelector('.elementexample_first')
+
+const $slementExamplefirst = document.querySelector('.elementExample_last')
+
+$slementExamplefirst.addeventlistener('click', function(event) {
+    this.classlist.toggle('elementExample_isactive')
+    $elementexamplefirst.classlist.add('elementExample_isactive')
+})
+
+$elementeny.elast.addeventlistener('click', function(event) {
+    this.classlist.toggle('elemetExample_isactive')
+    $elementexamplefirst.classlist.add('elementExample_isactive') 
+})
